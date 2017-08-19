7d40aeaff6058ed221aa8a1f22eca92296516238
John Brock <johnlrbrock@gmail.com>
2017-08-19 17:45:12 -0400

    linting

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+  const $content = $('content');
+  $content.html(`<div class="carousel">
+      <img class="left" src="http://placekitten.com/g/305/305">
+      <img class="center" src="http://placekitten.com/g/305/305">
+      <img class="right" src="http://placekitten.com/g/305/305">
+    </div>`);
+});
